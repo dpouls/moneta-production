3026eda295bf7f03ae402a67d49fc9797617b396
Dallin <dallinpoulson@gmail.com>
2021-08-16 14:13:57 -0600

    smoothing

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const express = require("express"),
 
 app.use(express.json());
 
-
+app.use(express.static(`${__dirname}/../build`))
 app.use(session({
     resave: false,
     saveUninitialized: true,
